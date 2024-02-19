e41bbf183803d62a3e80fe7d1eb35c59782c9856
Abdullah6979 <abdullahalmamun6979@gmail.com>
2024-02-19 06:55:12 +0600

    seat count section add in java script file

--- ticket.js
+++ ticket.js
@@ -1,16 +1,35 @@
+
 var selectedSeats = [];
+var seatNumber = 1;
 
 function selectSeat(seatId) {
     var seatButton = document.getElementById(seatId);
     if (seatButton.classList.contains('selected')) {
         seatButton.classList.remove('selected');
-        selectedSeats.splice(selectedSeats.indexOf(seatId), 1);
+        selectedSeats.splice(selectedSeats.indexOf(seatId));
+        
     } else {
         if (selectedSeats.length < 4) {
             seatButton.classList.add('bg-green-500');
             selectedSeats.push(seatId);
+            
         } else {
             alert('You can only select up to 4 seats.');
         }
     }
+    
+    updateSelectedSeats();
 }
+
+
+
+function updateSelectedSeats() {
+    var selectedSeatsElement = document.getElementById('selectedSeats');
+    selectedSeatsElement.textContent = selectedSeats.length;
+}
+
+function updateSeatNumber(seatButton) {
+    seatButton.textContent += " (Seat " + seatNumber + ")";
+    seatNumber++;
+}
+
